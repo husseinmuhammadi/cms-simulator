66bdb5b5322545d22322c995eae8813a45f604b2
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2020-12-23 18:51:41 +0330

    sim

--- web-app/src/test/java/com/asan/cms/ApplicationTest.java
+++ web-app/src/test/java/com/asan/cms/ApplicationTest.java
@@ -1,12 +1,13 @@
 package com.asan.cms;
 
+import com.asan.cms.web.controller.HomeController;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 // todo : replace datasource with h2 during test
 @SpringBootTest
-@ContextConfiguration
+@ContextConfiguration(classes = HomeController.class)
 class ApplicationTest {
 
     @Test
